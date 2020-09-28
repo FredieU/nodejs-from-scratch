f8d3a1a0f5eeed1447624545d5fa16f0f6228313
Fredie <contact@funabia.com>
2020-09-28 22:43:59 +0100

    Add parsing of request URL and logging requests to server

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const http = require('http');
+const url = require('url');
 
 const server = http.createServer((req, res) => {
+  const parsedUrl = url.parse(req.url, true);
+  const path = parsedUrl.pathname;
+  const trimmedPath = path.replace(/^\/+|\/+$/g, '');
+
   res.end('BOOP\n');
+
+  console.log(`Request to ${trimmedPath}`);
 });
 
 server.listen(3000, () => console.log('\nListening on port 3000...'));
